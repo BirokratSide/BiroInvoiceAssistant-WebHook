bf545d5836f5d398034bcb5a2f01e658d1655d25
kristijan.mirceta <kristijan.mirceta@gmail.com>
2018-04-20 09:00:58 +0200

    HTTPS traffic enabled. Problem because of self-signed certificate.

---
+++ InvoiceAssistantWebhook/Controllers/InvoiceAssistantController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InvoiceAssistantWebhook.Controllers
+{
+    [Route("api/[controller]")]
+    public class InvoiceAssistantController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "This method is accessible" };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public dynamic Post([FromBody] dynamic value)
+        {
+            Console.WriteLine();
+            dynamic neki = new { value = "some" };
+            //System.IO.File.WriteAllText("/Users/km/Desktop/invapireturn.txt", value.neki);
+            return neki;
+        }
+    }
+}
---
+++ InvoiceAssistantWebhook/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+using Newtonsoft.Json;
+
+namespace InvoiceAssistantWebhook
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        #region // config methods //
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    "AllowAllOrigins",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
+            services
+                .AddMvcCore()
+                .AddViews()
+                .AddRazorViewEngine()
+                .AddFormatterMappings()
+                .AddJsonFormatters()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.DateFormatString = "yyyy-MM-ddTHH:mm:ssZ";
+                    options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+                });
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseCors("AllowAllOrigins");
+            app.UseStaticFiles();
+            ConfigureServerToUseAndRedirectToHttps(app, env);
+            app.UseMvcWithDefaultRoute();
+        }
+        #endregion
+
+        #region [auxiliary]
+        private void ConfigureServerToUseAndRedirectToHttps(IApplicationBuilder app, IHostingEnvironment env) {
+            int? httpsPort = null;
+            var httpsSection = Configuration.GetSection("HttpServer:Endpoints:Https");
+            if (httpsSection.Exists())
+            {
+                var httpsEndpoint = new EndpointConfiguration();
+                httpsSection.Bind(httpsEndpoint);
+                httpsPort = httpsEndpoint.Port;
+            }
+            var statusCode = env.IsDevelopment() ? StatusCodes.Status302Found : StatusCodes.Status301MovedPermanently;
+            app.UseRewriter(new RewriteOptions().AddRedirectToHttps(statusCode, httpsPort));
+        }
+
+        #endregion
+    }
+}
